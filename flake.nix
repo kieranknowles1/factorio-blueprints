fa2dc8aad72b1e86fcc23ad19d961dd85ceffd0a
Kieran <kieranknowles11@hotmail.co.uk>
2024-10-31 22:38:39 +0000

    Replace with initial 2.0 blueprints

--- flake.nix
+++ flake.nix
@@ -11,6 +11,23 @@
       url = "github:kieranknowles1/nixcfg";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
+
+      # Disable inputs not needed outside of nixcfg
+      # These can be re-enabled if needed
+      inputs.clan-core.follows = "";
+      inputs.factorio-blueprints.follows = "";
+      inputs.firefox-addons.follows = "";
+      inputs.flake-utils-plus.follows = "";
+      inputs.home-manager.follows = "";
+      inputs.nix-index-database.follows = "";
+      inputs.nixos-cosmic.follows = "";
+      inputs.nixpkgs-openmw.follows = "";
+      inputs.nixpkgs-stable.follows = "";
+      inputs.nixvim.follows = "";
+      inputs.openmw.follows = "";
+      inputs.snowfall-lib.follows = "";
+      inputs.sops-nix.follows = "";
+      inputs.stylix.follows = "";
     };
   };
 
@@ -33,7 +50,7 @@
             export = lib.getExe inputs'.nixcfg.packages.export-blueprints;
           in
             pkgs.writeShellScriptBin "export" ''
-              ${export} ./blueprints
+              ${export} ./blueprints text
             '';
         };
       };
